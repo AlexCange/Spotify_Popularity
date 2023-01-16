ad471a258e7c55d4073df4901119abbbb0bd8f3f
holdsworth88 <automorphism88@gmail.com>
2023-01-16 14:44:57 +0100

    updated audio functions to add crop and reduce

--- audio_functions.py
+++ audio_functions.py
@@ -1,3 +1,5 @@
+#%%
+
 def retrieve_mp3s():
     for i in range(len(Omega_df)):
         if Omega_df['Track_URL'][i] is not None:
@@ -39,3 +41,35 @@ def make_spectrograms(output_path):
 def visualize_waveform():
     y, sr = librosa.load(r".\wavs\00Blm7zeNqgYLPtW6zg8cj.wav",duration=30)
     spec = librosa.feature.melspectrogram(y=y, sr=sr)
+
+
+def crop_image(image_path, output_path):
+    from PIL import Image
+    image_name = image_path.split('\\')[-1]
+    im = Image.open(image_path)
+    left, top, right, bottom = 80, 60, 575, 425
+    im1 = im.crop((left, top, right, bottom))
+    im1.save(output_path + image_name)
+
+def reduce_image_size(input_path, output_path):
+    from PIL import Image
+    image_name = input_path.split('\\')[-1]
+    im = Image.open(input_path)
+    im = im.resize((240,320),Image.ANTIALIAS)
+    im.save(output_path + image_name, optimize=True, quality=70)
+
+
+## Fixing the npz dataframe
+
+# for i in range(len(merged_df['Path'])):
+#     merged_df['Path'][i] = data_dir + merged_df['Path'].str.split('\\')[i][-1]
+#     if i % 100 ==0:
+#         print(i)
+
+# for i in range(len(merged_df['NPZ_Path'])):
+#     merged_df['NPZ_Path'][i] = data_dir + merged_df['NPZ_Path'].str.split('\\')[i][-1]
+#     if i % 1000 ==0:
+#         print(i)
+
+
+# %%
